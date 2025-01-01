c85dd99199e186f771a7768a99a5e6e7a43f77b6
Kanchan Kuwar <kanchan.kuwar@ignitesol.com>
2025-01-01 22:00:01 +0530

    refactor sqlparser

--- sql_parser.py
+++ sql_parser.py
@@ -2,58 +2,55 @@ import sqlparse
 from sqlparse.tokens import Keyword
 from sqlparse.sql import Identifier,Parenthesis,IdentifierList,Function
 
-def parse_create_table(query):
-    parsed = sqlparse.parse(query)[0]
-    tokens = parsed.tokens
-    table_seen = False
-    table_name_Seen = False
-    TABLE_NAME = None
-    # print(tokens)
-    for t in tokens:
-        if(t.ttype is Keyword and t.value.upper() == 'TABLE'):
-            table_seen = True
-        if(table_seen and isinstance(t, Identifier)):
-            table_name_Seen = True
-            TABLE_NAME = t.get_real_name()
-            print("Table_name",t.get_real_name())
-        if(isinstance(t,Parenthesis)):
-            result = []        
-            for i in t.tokens:
-                if(isinstance(i,Identifier)):
-                    result.append(i.get_real_name())
-                if(isinstance(i,IdentifierList)):
-                    for j in i.get_identifiers():
-                        if(isinstance(j,Identifier)):
-                            result.append(j.get_real_name())
-                        if(isinstance(j,Function)):
-                            result.append(j.value)
-                        if(j.value == 'int'):
-                            result.append(j.value)
-                        if(j.value == 'class'):
-                            result.append(j.value)
+def check_instance_of_token(token):
+    val = None
+    if(isinstance(token,Identifier)):
+        val = token.get_real_name()
+    if(isinstance(token,Function) or token.value in keywords):
+        val = token.value
+    
+    return val
 
-                if(isinstance(i,Function)):
-                    result.append(i.value)
+def append_token_values(result,token):
+    val = check_instance_of_token(token)
+    if(isinstance(i,IdentifierList)):
+        for j in i.get_identifiers():
+            val = check_instance_of_token(j)
+            result.append(val)
+    elif(val):
+        result.append(val)
 
-                if(i.value == 'int'):
-                    result.append(i.value)
-                if(i.value == 'class'):
-                    result.append(i.value)
+def generate_mermaid_er(table_name, columns):
+    er_diagram = f"""erDiagram
+    {table_name} {{
+    """
+    for column, data_type in columns:
+        er_diagram += f"        {data_type} {column}\n"
+    er_diagram += "    }\n"
+    return er_diagram
 
-    result_tuple_list=[]
-    for i in range(0,len(result),2):
-        result_tuple_list.append((result[i],result[i+1]))
+tokens = (sqlparse.parse('create table Chaturji(chat varchar(30), users int, cost double);')[0]).tokens
+table_seen = False
+TABLE_NAME = None
+keywords = ['int', 'class', 'double']
 
-    print("Result tuple list",result_tuple_list)
+for t in tokens:
+    if(t.ttype is Keyword and t.value.upper() == 'TABLE'):
+        table_seen = True
+    if(table_seen and isinstance(t, Identifier)):
+        TABLE_NAME = t.get_real_name()
+    if(isinstance(t,Parenthesis)):
+        result = []     
+        val = None
+        for i in t.tokens:
+            val = None
+            append_token_values(result,i)
 
-    def generate_mermaid_er(table_name, columns):
-        er_diagram = f"""erDiagram
-        {table_name} {{
-        """
-        for column, data_type in columns:
-            er_diagram += f"        {data_type} {column}\n"
-        er_diagram += "    }\n"
-        return er_diagram
-
-    mermaid_er = generate_mermaid_er(TABLE_NAME, result_tuple_list)
-    return mermaid_er
+print("final result",result)
+result_tuple_list=[]
+for i in range(0,len(result),2):
+    result_tuple_list.append((result[i],result[i+1]))
+    
+print("Result tuple list",result_tuple_list)
+mermaid_er = generate_mermaid_er(TABLE_NAME, result_tuple_list)
+print(mermaid_er)
